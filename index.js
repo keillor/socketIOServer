10fa6326642822e650fc90d22f02896204ec8043
keillor <69380836+keillor@users.noreply.github.com>
2025-04-23 10:56:56 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+    
+  }
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log('a user connected');
+  io.emit('message', 'a user connected');
+
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected')
+  })
+
+  socket.on("message", (msg) => {
+    console.log("message recieved:", msg);
+    io.emit("message", msg);
+  });
+});
+
+
+server.listen(3000, () => {
+  console.log("server running!");
+});
