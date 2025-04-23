218ddda72bb93bb45bdb724cc63b85114286e3a3
keillor <69380836+keillor@users.noreply.github.com>
2025-04-23 10:57:25 -0700

    any origin allowed

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
-    
+
   }
 });
 
