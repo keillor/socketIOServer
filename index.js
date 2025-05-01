17e65d362513a21c8a6e081af8fcec7af1aed7fd
keillor <69380836+keillor@users.noreply.github.com>
2025-05-01 10:57:14 -0700

    any origin

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ import express from "express";
   const server = createServer(app);
   const io = new Server(server, {
     cors: {
-      origin: "https://guild-table.98.148.238.215.sslip.io/",
+      origin: "*",
       credentials: true,
     }
   });
